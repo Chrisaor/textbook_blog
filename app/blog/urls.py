8c4a2153d3072a2ba2134d85c3e40c497e9c3bfe
chrisaor <pchpch0070@gmail.com>
2018-06-22 23:00:19 +0900

    post_all.html작성

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from blog.views import PostLV, PostDV, PostAV, PostYAV, PostMAV, PostDAV, PostTAV
+
+
+app_name = 'blog'
 urlpatterns = [
     path('', PostLV.as_view(), name='index'),
     path('post/', PostLV.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDV.as_view(), name='post_detail'),
+    path('post/<str:slug>/', PostDV.as_view(), name='post_detail'),
     path('archive/', PostAV.as_view(), name='post_archive'),
     path('<int:year>/', PostYAV.as_view(), name='post_year_archive'),
     path('<int:year>/<int:month>/', PostMAV.as_view(), name='post_month_archive'),
